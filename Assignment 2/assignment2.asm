076f012826fe8b3a83695d2ae1c8fc03ef024c4b
aisha0139 <143417532+aisha0139@users.noreply.github.com>
2026-03-19 20:54:05 +0100

    finished assignment 2

---
+++ Assignment 2/assignment2.asm
@@ -0,0 +1,29 @@
+.ORIG x3000
+
+        JSR readS ; Call subroutine
+        HALT ; stop program after returning
+
+readS
+        LEA R0, PROMPT
+        PUTS ; Print message asking for input
+
+        GETC
+        OUT
+        LD R1, OFFSET
+        ADD R2, R0, R1 ; Read first digit and convert it from ASCII to its actual numerical value
+
+        GETC
+        OUT
+        ADD R0, R0, R1 ; Read second digit and convert it from ASCII to its actual numerical value 
+     
+       ; Multiply first digit by 10 using repeated addition
+        ADD R2, R2, R2     ; 2 * first digit
+        ADD R3, R2, R2     ; 4 * first digit
+        ADD R3, R3, R3     ; 8 * first digit
+        ADD R4, R3, R2     ; 10 * first digit
+        
+        RET ; return to main 
+
+OFFSET  .FILL #-48 ;  ASCII offset for converting a character digit into an integer
+PROMPT  .STRINGZ "Input a 2 digit decimal number:"
+.END
